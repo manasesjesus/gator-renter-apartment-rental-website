acc0ccb1285d830dcb0e905203890253b7d8e87f
jiriloffelmann <loffelmann.j@gmail.com>
2016-10-24 11:51:24 -0700

    updating service url

--- public/components/apartment/apartment.service.js
+++ public/components/apartment/apartment.service.js
@@ -1,5 +1,5 @@
 app.factory('Apartment', ['$resource', function($resource) {
-	return userResource = $resource('/gator-renter/api/apartment', {
+	return userResource = $resource('/~f16g08/api/apartment', {
 		apartment_id: '@apartment_id'
 	});
 }]);
