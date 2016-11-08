ef4de9141ac810e973eea37c1a973098b1d8686a
jiriloffelmann <loffelmann.j@gmail.com>
2016-11-07 17:59:52 -0800

    updating apt service

--- public/components/apartment/apartment.service.js
+++ public/components/apartment/apartment.service.js
@@ -1,5 +1,5 @@
 app.factory('Apartment', ['$resource', function($resource) {
-	return userResource = $resource('/api/apartment', {
+	return userResource = $resource('/~f16g08/api/apartment', {
 		apartment_id: '@apartment_id'
 	});
 }]);
