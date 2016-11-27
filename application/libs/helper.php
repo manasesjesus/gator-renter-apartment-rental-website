d9c541e1db949e4e4869e6868f0f500aabba687c
Intesar Haider <intesar.haider1@gmail.com>
2016-11-27 02:41:58 +0100

    Revert "Merge pull request #2 from intesar-gist/feature/fulda-development"
    This reverts commit d06861d55249093659a6f19e3bcf2d013d44234d, reversing
    changes made to ce7b972e1efd1c0832d2cefd4caa24453e00240f.

--- application/libs/helper.php
+++ application/libs/helper.php
@@ -52,25 +52,4 @@ class Helper
         return $raw_sql;
     }
 
-    public static function getCurrentMySQLFormatTime() {
-        return date("Y-m-d H:i:s", time());
-    }
-
-    public static function encryptPassword($password) {
-
-        // A higher "cost" is more secure but consumes more processing power
-        $cost = 10;
-
-        // Create a random salt
-        $salt = strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.');
-
-        // Prefix information about the hash so PHP knows how to verify it later.
-        // "$2a$" Means we're using the Blowfish algorithm. The following two digits are the cost parameter.
-        $salt = sprintf("$2a$%02d$", $cost) . $salt;
-
-        // Hash the password with the salt
-        return crypt($password, $salt);
-
-    }
-
 }
--- resources/db_folder/README_db_construct.txt
+++
@@ -1,6 +0,0 @@
-1. run gatorRenter_auth.sql to create database mini and create table 'apartments'
-2. use mysql workbench or phpadmin to import the data in file "mini_apartments" into table "apartments"
-	2.1 please note that Line sperator is "LF",field separator is ","
-	2.2 Information needed is in file "export_settings.png"
-3. run "dummy_user_db.sql" to create and import user data in the database mini
-4. database is settled and please edit the config.php file 
