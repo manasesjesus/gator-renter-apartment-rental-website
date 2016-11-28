a0ba1b86a543a89a2d07e11a36cbfab824fee226
Alex Aichinger <aichinger.alex24315@gmail.com>
2016-11-27 18:04:43 -0800

    Created in home.ctrl.js a function to retrieve stored user_id. Started implementation of profile page logic.

--- public/components/home/home.ctrl.js
+++ public/components/home/home.ctrl.js
@@ -49,6 +49,10 @@ app.controller('homeController', ['$location', '$scope', '$rootScope', 'store', 
 		return store.get('profile') != null ? store.get('profile')['username'] : '';
 	};
 
+	$rootScope.getUserID = function() {
+		return store.get('profile') != null ? store.get('profile')['user_id'] : '';
+	};
+
 	$rootScope.postApartment = function() {
 		if($rootScope.isAuthenticated()) {
 			$rootScope.showPost = true;
