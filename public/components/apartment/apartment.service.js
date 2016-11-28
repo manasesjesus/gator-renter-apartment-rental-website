1ef56f0a535a6569f2eab2e0f41ee001eff288d8
Manasés <manases@mail.com>
2016-11-28 21:36:17 +0100

    - User login using User API and password encryption - Post Apartment uses real user ID

--- public/components/apartment/apartment.service.js
+++ public/components/apartment/apartment.service.js
@@ -1,5 +1,5 @@
 app.factory('Apartment', ['$resource', function($resource) {
-	return userResource = $resource('/api/apartment', {
+	return userResource = $resource('api/apartment', {
 		apartment_id: '@apartment_id'
 	});
 }]);
