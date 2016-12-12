e418ea30a304fd8706429aa21efdffb079ae83d9
Manasés <manases@mail.com>
2016-12-12 05:35:59 -0600

    New functions for profile administration / Formatting code using spaces instead of tabs

--- public/components/home/home.ctrl.js
+++ public/components/home/home.ctrl.js
@@ -1,52 +1,52 @@
-app.controller('homeController', ['$location', '$scope', '$rootScope', 'store', 'Apartment', 'Upload', '$http', function($location, $scope, $rootScope, store, Apartment, Upload, $http) {
+app.controller('homeController', ['$location', '$scope', '$rootScope', 'store', 'Apartment', 'Upload', '$http', function ($location, $scope, $rootScope, store, Apartment, Upload, $http) {
 
-	$scope.showPreloader = true;
-	$scope.loadingOwnerApartments = true;
+    $scope.showPreloader = true;
+    $scope.loadingOwnerApartments = true;
 
-    Apartment.query().$promise.then(function(data) {
-    	$scope.showPreloader = false;
-    	$scope.originalApartments = data;
-    	$scope.apartments = $scope.originalApartments;
+    Apartment.query().$promise.then(function (data) {
+        $scope.showPreloader = false;
+        $scope.originalApartments = data;
+        $scope.apartments = $scope.originalApartments;
     });
 
-    if(store.get('profile') != null) {
-    	Apartment.query({ owner_id: store.get('profile')['user_id'] }).$promise.then(function(data) {
-	    	$scope.ownerApartments = data;
-	    	$scope.loadingOwnerApartments = false;
-	    });
+    if (store.get('profile') != null) {
+        Apartment.query({owner_id: store.get('profile')['user_id']}).$promise.then(function (data) {
+            $scope.ownerApartments = data;
+            $scope.loadingOwnerApartments = false;
+        });
     }
 
     $rootScope.newApt = {};
     $rootScope.errorFields = undefined;
 
-	$rootScope.showLogin = false;
-	$rootScope.showSignup = false;
-	$rootScope.showPost = false;
+    $rootScope.showLogin = false;
+    $rootScope.showSignup = false;
+    $rootScope.showPost = false;
 
-	$scope.propertyName = 'id';
-	$scope.reverse = true;
+    $scope.propertyName = 'id';
+    $scope.reverse = true;
 
-	$scope.view = 'list';
+    $scope.view = 'list';
 
-	$scope.privateRoom = false;
-	$scope.privateBath = false;
-	$scope.kitchenIn = false;
-	$scope.noDeposit = false;
-	$scope.noCredit = false;
+    $scope.privateRoom = false;
+    $scope.privateBath = false;
+    $scope.kitchenIn = false;
+    $scope.noDeposit = false;
+    $scope.noCredit = false;
 
-	/* Methods */
+    /* Methods */
 
-	$rootScope.login = function() {
-		var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		if (regex.test($rootScope.username) && $rootScope.hasValidData($rootScope.password, 1)) {
+    $rootScope.login = function () {
+        var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (regex.test($rootScope.username) && $rootScope.hasValidData($rootScope.password, 1)) {
 
             $http({
                 url: 'api/login',
                 method: "POST",
-                data: {loginname:$rootScope.username, password:$rootScope.password},
-                transformRequest: function(obj) {
+                data: {loginname: $rootScope.username, password: $rootScope.password},
+                transformRequest: function (obj) {
                     var str = [];
-                    for(var p in obj)
+                    for (var p in obj)
                         str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
                     return str.join("&");
                 },
@@ -54,34 +54,35 @@ app.controller('homeController', ['$location', '$scope', '$rootScope', 'store', 
                     "Content-Type": "application/x-www-form-urlencoded"
                 }
             }).then(function successCallback(response) {
-                    // this callback will be called asynchronously
-                    if (response.data.first_name) {
-                        store.set('profile', {username: $rootScope.username, user_id: response.data.uid});
-                        $rootScope.loginMessage = '';
-                        $rootScope.showLogin = false;
-                    }
-                    else {
-                        $rootScope.loginMessage = 'email or password incorrect';
-                    }
+                // this callback will be called asynchronously
+                if (response.data.first_name) {
+                    $rootScope.setProfile($rootScope.username, response.data.uid,
+                        response.data.first_name + " " + response.data.last_name, response.data.user_roles_id);
+                    $rootScope.loginMessage = '';
+                    $rootScope.showLogin = false;
+                }
+                else {
+                    $rootScope.loginMessage = 'email or password incorrect';
+                }
             }, function errorCallback(response) {
-                    // called asynchronously if an error occurs
-                    $rootScope.loginMessage = 'Error: ' + response;
+                // called asynchronously if an error occurs
+                $rootScope.loginMessage = 'Error: ' + response;
             });
 
-		} else {
-			$rootScope.loginMessage = 'email or password incorrect';
-		}
-	};
+        } else {
+            $rootScope.loginMessage = 'email or password incorrect';
+        }
+    };
 
-	$rootScope.logout = function() {
-        
+    $rootScope.logout = function () {
+
         $http({
             url: 'api/logout',
             method: "POST"
         }).then(function successCallback(response) {
             // this callback will be called asynchronously
             if (response) {
-                if(response === 'SUCCESS_LOGOUT') {
+                if (response === 'SUCCESS_LOGOUT') {
                     $rootScope.loginMessage = 'Successfully Logged out';
                 }
             } else {
@@ -100,19 +101,41 @@ app.controller('homeController', ['$location', '$scope', '$rootScope', 'store', 
         $rootScope.password = "";
         $rootScope.address = "";
         $scope.go('/');
-	};
+    };
 
-	$rootScope.signup = function() {
+    $rootScope.setProfile = function (u1, u2, u3, u4) {
+        store.set('profile', {
+            username: u1,
+            user_id: u2,
+            user_fullname: u3,
+            user_role: u4
+        });
+    }
+
+    $rootScope.signup = function () {
         var errors = "";
         var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         $rootScope.errorFields = undefined;
         if (regex.test($rootScope.username)) {
             // Name validations, e.g. Xi Li (China)
-            if (!$rootScope.hasValidData($rootScope.first_name, 2)) { errors += "first_name, "; }
-            if (!$rootScope.hasValidData($rootScope.last_name, 2))  { errors += "last_name, "; }
+            if (!$rootScope.hasValidData($rootScope.first_name, 2)) {
+                errors += "first_name, ";
+            }
+            if (!$rootScope.hasValidData($rootScope.last_name, 2)) {
+                errors += "last_name, ";
+            }
             // Simple password, not strict
-            if (!$rootScope.hasValidData($rootScope.password, 1))   { errors += "password"; }
+            if (!$rootScope.hasValidData($rootScope.password, 1)) {
+                errors += "password, ";
+            }
+            // Address and city
+            if (!$rootScope.hasValidData($rootScope.address, 5)) {
+                errors += "address, ";
+            }
+            if (!$rootScope.hasValidData($rootScope.city, 2)) {
+                errors += "city";
+            }
 
             if (errors.length == 0) {
                 $http({
@@ -128,11 +151,13 @@ app.controller('homeController', ['$location', '$scope', '$rootScope', 'store', 
                         role_type_id: "2"
                     }
                 }).then(function successCallback(response) {
-                    store.set('profile', {username: $rootScope.username, user_id: response.data.data.user_id});
+                    //store.set('profile', {username: $rootScope.username, user_id: response.data.data.user_id});
+                    $rootScope.setProfile($rootScope.username, response.data.uid,
+                        response.data.first_name + " " + response.data.last_name, response.data.user_roles_id);
                     $rootScope.loginMessage = '';
                     $rootScope.showSignup = false;
                 }, function errorCallback(response) {
-                    $rootScope.loginMessage = 'Error: ' + response;
+                    $rootScope.loginMessage = response.data.reason;
                 });
             }
             else {
@@ -143,111 +168,119 @@ app.controller('homeController', ['$location', '$scope', '$rootScope', 'store', 
             $rootScope.errorFields = "username";
             $rootScope.loginMessage = 'Please provide a valid email address';
         }
-	};
+    };
 
-	$rootScope.hasValidData = function (field, lng) {
+    $rootScope.hasValidData = function (field, lng) {
         return (field && field.length >= lng);
     }
 
-	$rootScope.isAuthenticated = function() {
-		return store.get('profile') != null;
-	};
+    $rootScope.isAuthenticated = function () {
+        return store.get('profile') != null;
+    };
 
-	$rootScope.getEmail = function() {
-		return store.get('profile') != null ? store.get('profile')['username'] : '';
-	};
+    $rootScope.getProfilePage = function () {
+        return store.get('profile') != null ? (store.get('profile')['user_role'] == 1 ? '#/admin' : '#/profile') : '';
+    };
 
-	$rootScope.postApartment = function() {
-		if($rootScope.isAuthenticated()) {
-			$rootScope.showPost = true;
-		} else {
-			$rootScope.loginMessage = 'Please sign in to post an apartment!'
-			$rootScope.showLogin = true;
-		}
-	};
+    $rootScope.getFullName = function () {
+        return store.get('profile') != null ? store.get('profile')['user_fullname'] : '';
+    };
 
-	$rootScope.applyNow = function() {
-		if($rootScope.isAuthenticated()) {
-			$rootScope.showApplyNow = true;
-		} else {
-			$rootScope.loginMessage = 'Please sign in to apply!'
-			$rootScope.showLogin = true;
-		}
-	};
+    $rootScope.getEmail = function () {
+        return store.get('profile') != null ? store.get('profile')['username'] : '';
+    };
 
-	$rootScope.savePost = function() {
-		$rootScope.newApt['owner_id'] = store.get('profile')['user_id'];
-		Apartment.save($rootScope.newApt, function(data) {
-			$rootScope.showPost = false;
-			$rootScope.newApt = {};
-			$scope.apartments.push(data);
-	    }, function(data) {
-	    	$rootScope.errorFields = data.data.error;
-	    });
-	};
+    $rootScope.postApartment = function () {
+        if ($rootScope.isAuthenticated()) {
+            $rootScope.showPost = true;
+        } else {
+            $rootScope.loginMessage = 'Please sign in to post an apartment!'
+            $rootScope.showLogin = true;
+        }
+    };
 
-	$rootScope.deleteApartment = function(apartment_id) {
-		if (confirm("Are you sure to remove this apartment offer?")) {
-			Apartment.delete({ id: apartment_id }, function() {
-				$scope.ownerApartments.splice($scope.ownerApartments.indexOf(apartment_id), 1);
-			});
-		}
-	};
+    $rootScope.applyNow = function () {
+        if ($rootScope.isAuthenticated()) {
+            $rootScope.showApplyNow = true;
+        } else {
+            $rootScope.loginMessage = 'Please sign in to apply!'
+            $rootScope.showLogin = true;
+        }
+    };
 
-	$scope.uploadPic = function(file) {
-		file.upload = Upload.upload({
-			url: '/api/upload',
-			data: { file: file }
-		});
-		file.upload.then(function(response) {
-			file.result = response.data;
-			if($rootScope.newApt.pictures == undefined) {
-				$rootScope.newApt.pictures = response.data.files;
-			} else {
-				$rootScope.newApt.pictures.push(response.data.files);
-			}
-		}, function(response) {
-			if(response.status > 0) {
-				$scope.errorMsg = response.status + ': ' + response.data;
-			}
-			}, function(evt) {
-				file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-			}
-		);
-	};
+    $rootScope.savePost = function () {
+        $rootScope.newApt['owner_id'] = store.get('profile')['user_id'];
+        Apartment.save($rootScope.newApt, function (data) {
+            $rootScope.showPost = false;
+            $rootScope.newApt = {};
+            $scope.apartments.push(data);
+        }, function (data) {
+            $rootScope.errorFields = data.data.error;
+        });
+    };
 
-	$scope.update = function() {
-		if($scope.minPrice == '') $scope.minPrice = undefined;
-		if($scope.maxPrice == '') $scope.maxPrice = undefined;
-		if($scope.minPrice == undefined && $scope.maxPrice == undefined) {
-			$scope.apartments = $scope.originalApartments;	
-		} else {
-			$scope.apartments = [];
-			if($scope.minPrice == undefined && $scope.maxPrice != undefined) {
-				var min = 0;
-				var max = parseFloat($scope.maxPrice);
-			} else if($scope.minPrice != undefined && $scope.maxPrice == undefined) {
-				var min = parseFloat($scope.minPrice);
-				var max = 100000;
-			} else {
-				var min = parseFloat($scope.minPrice);
-				var max = parseFloat($scope.maxPrice);
-			}
-			angular.forEach($scope.originalApartments, function(apartment, key) {
-				if(apartment.monthly_rent >= min && apartment.monthly_rent <= max) {
-					$scope.apartments.push(apartment);
-				}
-			});
-		}
-	};
+    $rootScope.deleteApartment = function (apartment_id) {
+        if (confirm("Are you sure to remove this apartment offer?")) {
+            Apartment.delete({id: apartment_id}, function () {
+                $scope.ownerApartments.splice($scope.ownerApartments.indexOf(apartment_id), 1);
+            });
+        }
+    };
 
-	$scope.sortBy = function(propertyName) {
-		$scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
-		$scope.propertyName = propertyName;
-	};
+    $scope.uploadPic = function (file) {
+        file.upload = Upload.upload({
+            url: '/api/upload',
+            data: {file: file}
+        });
+        file.upload.then(function (response) {
+                file.result = response.data;
+                if ($rootScope.newApt.pictures == undefined) {
+                    $rootScope.newApt.pictures = response.data.files;
+                } else {
+                    $rootScope.newApt.pictures.push(response.data.files);
+                }
+            }, function (response) {
+                if (response.status > 0) {
+                    $scope.errorMsg = response.status + ': ' + response.data;
+                }
+            }, function (evt) {
+                file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            }
+        );
+    };
 
-	$scope.go = function(path) {
-		$location.path(path);
-	};
+    $scope.update = function () {
+        if ($scope.minPrice == '') $scope.minPrice = undefined;
+        if ($scope.maxPrice == '') $scope.maxPrice = undefined;
+        if ($scope.minPrice == undefined && $scope.maxPrice == undefined) {
+            $scope.apartments = $scope.originalApartments;
+        } else {
+            $scope.apartments = [];
+            if ($scope.minPrice == undefined && $scope.maxPrice != undefined) {
+                var min = 0;
+                var max = parseFloat($scope.maxPrice);
+            } else if ($scope.minPrice != undefined && $scope.maxPrice == undefined) {
+                var min = parseFloat($scope.minPrice);
+                var max = 100000;
+            } else {
+                var min = parseFloat($scope.minPrice);
+                var max = parseFloat($scope.maxPrice);
+            }
+            angular.forEach($scope.originalApartments, function (apartment, key) {
+                if (apartment.monthly_rent >= min && apartment.monthly_rent <= max) {
+                    $scope.apartments.push(apartment);
+                }
+            });
+        }
+    };
+
+    $scope.sortBy = function (propertyName) {
+        $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+        $scope.propertyName = propertyName;
+    };
+
+    $scope.go = function (path) {
+        $location.path(path);
+    };
 
 }]);
