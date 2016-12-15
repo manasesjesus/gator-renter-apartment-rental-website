86f2a6ecab591ce2a0764eaa1b7875b8f12d195b
Manasés <manases@mail.com>
2016-12-15 02:20:21 -0600

    changed SP to use UTC_TIME and correct message size

--- resources/db_folder/2.0 - internal_messages/4.0 - saveNewMessage procedure.sql
+++ resources/db_folder/2.0 - internal_messages/4.0 - saveNewMessage procedure.sql
@@ -1,16 +1,17 @@
+
+DELIMITER ;
+
 DROP procedure IF EXISTS `addMessage`;
 
 DELIMITER $$
-CREATE PROCEDURE `addMessage`
-(in in_from_user_id varchar(50),
-in in_to_user_id varchar(255),
-in in_apartment_id varchar(255),
-in in_message varchar(50))
-BEGIN
-	
-    INSERT INTO user_messages (from_user_id, to_user_id, apartment_id, message, created)
-    VALUES (in_from_user_id, in_to_user_id, in_apartment_id, in_message, UTC_TIME());
-    
-END$$
+
+CREATE PROCEDURE `addMessage`(in in_from_user_id varchar(50), in in_to_user_id varchar(255),
+                              in in_apartment_id varchar(255), in in_message varchar(1000))
+    BEGIN
+
+        INSERT INTO user_messages (from_user_id, to_user_id, apartment_id, message, created)
+        VALUES (in_from_user_id, in_to_user_id, in_apartment_id, in_message, UTC_TIMESTAMP());
+
+    END$$
 
 DELIMITER ;
