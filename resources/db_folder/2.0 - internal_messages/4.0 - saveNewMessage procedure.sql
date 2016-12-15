a6314ce86403be0f4d50668e3a03500590c2ccfa
Manny Einsteiger <manases@gmx.de>
2016-12-15 10:26:25 -0600

    fixing Store Procedure to work on heroku DB

--- resources/db_folder/2.0 - internal_messages/4.0 - saveNewMessage procedure.sql
+++ resources/db_folder/2.0 - internal_messages/4.0 - saveNewMessage procedure.sql
@@ -1,17 +1,13 @@
-
-DELIMITER ;
-
 DROP procedure IF EXISTS `addMessage`;
 
 DELIMITER $$
-
 CREATE PROCEDURE `addMessage`(in in_from_user_id varchar(50), in in_to_user_id varchar(255),
                               in in_apartment_id varchar(255), in in_message varchar(1000))
-    BEGIN
+  BEGIN
 
-        INSERT INTO user_messages (from_user_id, to_user_id, apartment_id, message, created)
-        VALUES (in_from_user_id, in_to_user_id, in_apartment_id, in_message, UTC_TIMESTAMP());
+    INSERT INTO user_messages (from_user_id, to_user_id, apartment_id, message, created)
+    VALUES (in_from_user_id, in_to_user_id, in_apartment_id, in_message, DATE_ADD(UTC_TIMESTAMP(), INTERVAL 1 HOUR));
 
-    END$$
+  END$$
 
 DELIMITER ;
